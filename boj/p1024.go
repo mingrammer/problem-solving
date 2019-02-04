421179c8bbbfa1531e2d6ac265c5ba84198c9758
mingrammer <mingrammer@gmail.com>
2019-02-04 19:17:59 +0900

    [BOJ] Solve the problem 1024

---
+++ boj/p1024.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var n, l float64
+	fmt.Scanln(&n, &l)
+
+	for k := l; ; k++ {
+		if k > 100 {
+			fmt.Print(-1)
+			break
+		}
+		m := n/k - (k-1)/2
+		if m < 0 {
+			fmt.Print(-1)
+			break
+		}
+		c := math.Floor(m)
+		if c == m {
+			a := []string{}
+			for i := m; i < m+k; i++ {
+				a = append(a, strconv.Itoa(int(i)))
+			}
+			fmt.Print(strings.Join(a, " "))
+			break
+		}
+	}
+}
