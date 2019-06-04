de22e5cb789f6c904950938530b91472a0c226c3
mingrammer <mingrammer@gmail.com>
2019-06-04 21:20:07 +0900

    Add a missing struct

--- leetcode/p2.go
+++ leetcode/p2.go
@@ -1,12 +1,10 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	next := new(ListNode)
 	l3 := next
