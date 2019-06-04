96c5f1700a1ced93156255e3af75fa90f9667a1e
mingrammer <mingrammer@gmail.com>
2019-06-04 21:19:36 +0900

    Remove unused

--- leetcode/p3.go
+++ leetcode/p3.go
@@ -3,7 +3,7 @@ package main
 func lengthOfLongestSubstring(s string) int {
 	ans := 0
 	m := map[byte]bool{}
-	for i, _ := range []byte(s) {
+	for i := range []byte(s) {
 		for _, d := range []byte(s[i:]) {
 			if _, ok := m[d]; ok {
 				break
