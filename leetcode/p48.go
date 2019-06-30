9f46ec77afecb5fe77e5fc6e0fd264de9dc43cbc
mingrammer <mingrammer@gmail.com>
2019-07-01 01:17:25 +0900

    [LeetCode] Solve the problem 48

---
+++ leetcode/p48.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-1-i; j++ {
+			t := matrix[i][j]
+			matrix[i][j] = matrix[n-1-j][i]
+			matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+			matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+			matrix[j][n-1-i] = t
+		}
+	}
+	fmt.Println(matrix)
+}
+
+func main() {
+	rotate([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	rotate([][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}})
+	rotate([][]int{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}})
+}
