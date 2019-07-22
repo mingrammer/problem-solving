74e7ec21577ca9af6ea697c898896eca1e274764
mingrammer <mingrammer@gmail.com>
2019-07-23 00:58:15 +0900

    [LeetCode] (206) Add recursion solution

--- leetcode/p206.go
+++ leetcode/p206.go
@@ -5,6 +5,18 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// recursion.
+// func reverseList(head *ListNode) *ListNode {
+// 	if head == nil || head.Next == nil {
+// 		return head
+// 	}
+// 	tail := reverseList(head.Next)
+// 	head.Next.Next = head
+// 	head.Next = nil
+// 	return tail
+// }
+
+// iteration.
 func reverseList(head *ListNode) *ListNode {
 	var rev *ListNode
 	cur := head
