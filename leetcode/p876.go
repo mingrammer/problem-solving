bd372f93cb2385353da2ce4e09a762bcf1a28ecf
mingrammer <mingrammer@gmail.com>
2019-07-29 14:47:45 +0900

    [LeetCode] Solve the problem 876

---
+++ leetcode/p876.go
@@ -0,0 +1,20 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	var i, j int
+	middle := head
+	for head != nil && head.Next != nil {
+		head = head.Next
+		i++
+		if j*2 < i {
+			j++
+			middle = middle.Next
+		}
+	}
+	return middle
+}
